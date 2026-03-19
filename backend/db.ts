2791c952aa01caf93487bf2add8657ae82ca7d6f
polofranco882 <polofranco882@github.com>
2026-03-19 00:18:30 -0500

    Force ignore SSL certificate errors in production

--- backend/db.ts
+++ backend/db.ts
@@ -6,6 +6,10 @@ import { Client } from 'pg';
 
 const isLocal = !process.env.DATABASE_URL || process.env.DATABASE_URL.includes('localhost') || process.env.DATABASE_URL.includes('127.0.0.1');
 
+if (!isLocal) {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+}
+
 const client = new Client({
     connectionString: process.env.DATABASE_URL || 'postgresql://postgres:sa@localhost:5432/TTE',
     ssl: isLocal ? false : { rejectUnauthorized: false }
