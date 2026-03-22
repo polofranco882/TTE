5469fe040f2fdd46f2913d04bb12f0fba6342d2e
polofranco882 <polofranco882@github.com>
2026-03-21 23:52:20 -0500

    CORRIGIENDO

--- backend/tmp_check_triggers.js
+++
@@ -1,8 +0,0 @@
-const { Client } = require('pg');
-require('dotenv').config();
-const client = new Client({ connectionString: process.env.DATABASE_URL });
-client.connect().then(async () => {
-    const res = await client.query("SELECT trigger_name FROM information_schema.triggers WHERE event_object_table = 'landing_videos'");
-    console.log(JSON.stringify(res.rows, null, 2));
-    client.end();
-}).catch(e => { console.error(e.message); process.exit(1); });
